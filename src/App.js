b2421eff6a1438045cd062276eedb11dd94ddf1b
foyez <foyezar@yahoo.com>
2019-12-04 20:46:34 +0600

    create full crud app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react'
 
-function App() {
+import UsersTable from './tables/UsersTable'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
+
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'tania' },
+    { id: 2, name: 'Sohel', username: 'sohel' },
+    { id: 3, name: 'Foyez', username: 'foyez' },
+  ]
+  const [users, setUsers] = useState(usersData)
+  const [editing, setEditing] = useState(false)
+
+  const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+    setEditing(false)
+  }
+
+  const editRow = user => {
+    setEditing(true);
+    setCurrentUser({ ...user })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          { editing ? (
+            <Fragment>
+              <h2>Edit user</h2>
+              <EditUserForm
+                editing={ editing }
+                setEditing={ setEditing }
+                currentUser={ currentUser }
+                updateUser={ updateUser }
+              />
+            </Fragment>
+          ) : (
+              <Fragment>
+                <h2>Add user</h2>
+                <AddUserForm addUser={ addUser } />
+              </Fragment>
+            ) }
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UsersTable
+            users={ users }
+            editRow={ editRow }
+            deleteUser={ deleteUser }
+          />
+        </div>
+      </div>
     </div>
   );
 }
